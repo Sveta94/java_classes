97285effb8d8ce3414ff3b1689c3d3fe84d328c8
Sveta Avetisyan <sveta.avetisyan94@gmail.com>
2019-06-22 01:53:52 +0300

    Several functions added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -4,7 +4,28 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, World!");
+   hello("Sveta");
+   hello("user");
+
+   double l = 5;
+   double a = 8;
+   double b = 9;
+
+    System.out.println("The area of a square with a side of " + l + " = " + area(l));
+
+    System.out.println("The area of a rectangle with sides of " + a + " and " +  b  + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody){
+    System.out.println("Hello " + somebody + "!");
+  }
+  public static double area (double len){
+    return len * len;
+
+  }
+  public static double area (double a, double b){
+    return a * b;
 
   }
 
