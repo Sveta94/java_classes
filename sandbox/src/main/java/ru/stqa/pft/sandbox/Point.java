11860fcd80d7c2cbd8aabac53ed8898c8a440b85
Sveta Avetisyan <sveta.avetisyan94@gmail.com>
2019-06-23 01:48:53 +0300

    Distanc between two points (task 2)

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,20 @@
+package sandbox.src.main.java.ru.stqa.pft.sandbox;
+
+public class Point {
+
+  double x, y;
+
+  Point(double x, double y){
+    this.x = x;
+    this.y = y;
+
+  }
+
+  public  double distanceBetweenPoints1(Point p2){
+    double xDif = Math.pow((this.x - p2.x),2);
+    double yDif = Math.pow((this.y - p2.y),2);
+    return Math.sqrt(xDif + yDif);
+  }
+
+
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -3,7 +3,18 @@ package sandbox.src.main.java.ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+ Point p1 = new Point(4,3);
+ Point p2 = new Point(3,-2);
 
+    System.out.println(distanceBetweenPoints(p1, p2));
+    System.out.println(p1.distanceBetweenPoints1(p2));
+
+  }
+
+  public static double distanceBetweenPoints(Point p1, Point p2){
+    double xDif = Math.pow((p1.x - p2.x),2);
+    double yDif = Math.pow((p1.y - p2.y),2);
+    return Math.sqrt(xDif + yDif);
   }
 
 }
