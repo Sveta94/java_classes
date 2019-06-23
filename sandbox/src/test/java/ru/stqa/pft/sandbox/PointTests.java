bcdef89ecccb2ab1ae06fb561455064e2d6ea130
Sveta Avetisyan <sveta.avetisyan94@gmail.com>
2019-06-23 23:54:13 +0300

    Tests for counting the distance between points

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -2,22 +2,22 @@ package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.sandbox.Point;
 
-public class PointTests {
-  @Test
-  public void pointTest(){
-    Point p1 = new Point(4,3);
-    Point p2 = new Point(3, -2);
-    Assert.assertEquals(p1.distanceBetweenPoints1(p2), 5.0990195135927845);
 
-  }
+  public class PointTests {
+    @Test
+    public void pointTest(){
+      Point p1 = new Point(4,3);
+      Point p2 = new Point(3, -2);
+      Assert.assertEquals(p1.distanceBetweenPoints1(p2), 5.0990195135927845);
 
-  @Test
-  public void pointTest1(){
-    Point p1 = new Point(8,2);
-    Point p2 = new Point(6, 15);
-    Assert.assertEquals(p1.distanceBetweenPoints1(p2), 13.152946437965905);
+    }
+
+    @Test
+    public void pointTest1(){
+      Point p1 = new Point(8,2);
+      Point p2 = new Point(6, 15);
+      Assert.assertEquals(p1.distanceBetweenPoints1(p2), 13.152946437965905);
 
     }
   }
