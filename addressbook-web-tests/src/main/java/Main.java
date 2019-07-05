ca6ccf34bbc3b5e1c616cac4f85f694edaa76725
Sveta Avetisyan <sveta.avetisyan94@gmail.com>
2019-07-05 17:34:04 +0300

    Chromedriver and IEDriver added

---
+++ addressbook-web-tests/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    String a = "public";
+    String b = a;
+    a = "rolex";
+    System.out.println(a + " " + b);
+
+  }
+}
--- addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru.stqa.pft.addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.AppManager;
@@ -10,7 +11,7 @@ public class TestBase {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    app.init();
+    app.init(BrowserType.CHROME);
   }
 
   @AfterMethod(alwaysRun = true)
