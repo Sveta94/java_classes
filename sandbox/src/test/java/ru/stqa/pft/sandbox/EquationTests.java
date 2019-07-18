11b4538da23f973e34302e681d002e8625ca1dee
Sveta Avetisyan <sveta.avetisyan94@gmail.com>
2019-07-18 21:34:12 +0300

    Equation Tests moved to sandbox

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -1,9 +1,8 @@
-package ru.stqa.pft.addressbook.tests;
+package ru.stqa.pft.sandbox;
 
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.appmanager.Equation;
 
 
 
