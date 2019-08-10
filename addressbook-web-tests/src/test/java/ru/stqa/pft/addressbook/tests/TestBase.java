0d408c5dd2377c44f00f2fd4b8571bc9bbeee176
Sveta94 <sveta.avetisyan94@gmail.com>
2019-08-10 18:25:57 +0300

    6.11 Logback library added, Logger added to TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.AppManager;
+import ru.stqa.pft.addressbook.tests.GroupTests.GroupCreationTest;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(GroupCreationTest.class);
 
   protected static final AppManager app = new AppManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -20,5 +30,20 @@ public class TestBase {
 
 
   }
+
+  @BeforeMethod (alwaysRun = true)
+
+  public void logTestStart(Method m, Object [] p){
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+
+  }
+
+  @AfterMethod(alwaysRun = true)
+
+  public void logTestStop(Method m){
+
+    logger.info("Stop test " + m.getName());
+  }
+
 
 }
